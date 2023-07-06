bb207cad2260a190e841941a7522fa677bdfc036
dylan wuu <dylanwuu@oulook.com>
2023-07-06 08:56:22 -0700

    exercise complete

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myAccount = new BankAccount();
+
+            Console.WriteLine("How much money you you like to deposit?");
+            var deposit = double.Parse(Console.ReadLine());
+
+            myAccount.Deposit(deposit);
+
+            Console.WriteLine("Your current balance is:");
+            Console.WriteLine($"${myAccount.GetBalance()}");
         }
     }
 }
